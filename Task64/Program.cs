29acab82236d2f74eef31e7d5089ebdc370d721c
Elizaveta <veta.sterh88@mail.ru>
2023-08-01 21:22:24 +0700

    Задача 64

---
+++ Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string Factorial(int n, int start)
+{
+    if (n <= start) return start.ToString();
+    return Factorial(n, start + 1) + ", " + start;
+}
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int n = GetInput("Введите число: ");
+int start = 1;  
+Console.WriteLine($"{Factorial(n , start )}");
+
+
+
